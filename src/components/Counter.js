989e873521371769399be374892daba868e7efb4
Raginee Upadhyaya <upadhyayaraginee@gmail.com>
2022-03-15 09:23:08 -0700

    migrating everything to redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,7 @@
 //import {Component} from 'react';
 import classes from './Counter.module.css';
 import { useSelector,useDispatch, connect } from 'react-redux';
+import { counterActions } from '../store/index'
 //import { render } from '@testing-library/react';
 
 const Counter = () => {
@@ -9,20 +10,20 @@ const Counter = () => {
   const show = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({type:'increment' })
+    dispatch(counterActions.increment())
   }
   
   const increaseHandler = () => {
-    dispatch({type:'increase', amount: 5 })
+    dispatch(counterActions.increase(5)) // {type: some_unique_identfier, payload: 5}
   }
 
 
   const decrementHandler = () => {
-    dispatch({type:'decrement' })
+    dispatch(counterActions.decrement)
   }
 
   const toggleCounterHandler = () => { 
-    dispatch({type: 'toggle'})
+    dispatch(counterActions.toggleCounter())
   };
 
   return (
